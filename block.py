225f485fa0c2a5cddedd6f6cb25450c008014e03
belindamov <belindamov@gmail.com>
2023-09-02 16:00:39 +1200

    added a method to move tetrominos:

--- block.py
+++ block.py
@@ -1,5 +1,6 @@
 from colours import *
 import pygame
+from position import *
 
 
 class Block:
@@ -9,10 +10,24 @@ class Block:
         self.cell_size = 30
         self.rotation_state = 0
         self.colours = Colours.get_cell_colours()
+        self.row_offset = 0
+        self.col_offset = 0
 
     def draw(self, screen):
-        tiles = self.cells[self.rotation_state]
+        tiles = self.get_cell_positions()
         for tile in tiles:
-            tile_rect = pygame.Rect(tile.column * self.cell_size + 1, tile.row * self.cell_size + 1, self.cell_size - 1,
+            tile_rect = pygame.Rect(tile.col * self.cell_size + 1, tile.row * self.cell_size + 1, self.cell_size - 1,
                                     self.cell_size - 1)
             pygame.draw.rect(screen, self.colours[self.type], tile_rect)
+
+    def move(self, rows, cols):
+        self.row_offset += rows
+        self.col_offset += cols
+
+    def get_cell_positions(self):
+        tiles = self.cells[self.rotation_state]
+        moved_tiles = []
+        for position in tiles:
+            new_position = Position(position.row + self.row_offset, position.col + self.col_offset)
+            moved_tiles.append(new_position)
+        return moved_tiles
--- blocks.py
+++ blocks.py
@@ -12,6 +12,7 @@ class LBlock(Block):
             2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)],
             3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)]
         }
+        self.move(0, 3)
 
 
 class JBlock(Block):
@@ -23,6 +24,7 @@ class JBlock(Block):
             2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 2)],
             3: [Position(0, 1), Position(1, 1), Position(2, 0), Position(2, 1)]
         }
+        self.move(0, 3)
 
 
 class IBlock(Block):
@@ -34,6 +36,7 @@ class IBlock(Block):
             2: [Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)],
             3: [Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)]
         }
+        self.move(-1, 3)
 
 
 class OBlock(Block):
@@ -42,6 +45,7 @@ class OBlock(Block):
         self.cells = {
             0: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)]
         }
+        self.move(0, 4)
 
 
 class SBlock(Block):
@@ -53,6 +57,7 @@ class SBlock(Block):
             2: [Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1)],
             3: [Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1)]
         }
+        self.move(0, 3)
 
 
 class TBlock(Block):
@@ -64,6 +69,7 @@ class TBlock(Block):
             2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 1)],
             3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)]
         }
+        self.move(0, 3)
 
 
 class ZBlock(Block):
@@ -75,4 +81,4 @@ class ZBlock(Block):
             2: [Position(1, 0), Position(1, 1), Position(2, 1), Position(2, 2)],
             3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 0)]
         }
-        
+        self.move(0, 3)
