d59772b5d0e940e26ea73473326cf63c603500b3
belindamov <belindamov@gmail.com>
2023-09-02 16:30:34 +1200

    can move tetrominos left, right, and down now

--- main.py
+++ main.py
@@ -17,6 +17,15 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                game.move_left()
+            elif event.key == pygame.K_RIGHT:
+                game.move_right()
+            elif event.key == pygame.K_DOWN:
+                game.move_down()
+
+    # fill in background with white, then draw the grid and current block on top of it
     screen.fill(background)
     game.draw(screen)
     pygame.display.update()
