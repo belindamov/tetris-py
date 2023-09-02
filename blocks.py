cd5a6fddc5e35f638c91f891e6162f02baa4626f
belindamov <belindamov@gmail.com>
2023-09-02 15:41:34 +1200

    made all tetrominos and their respective rotations

--- blocks.py
+++ blocks.py
@@ -5,9 +5,74 @@ from position import *
 class LBlock(Block):
     def __init__(self):
         super().__init__(block_type=1)
+        # 0 is no rotation; increment of 1 means 90-degree rotation from the previous rotation state
         self.cells = {
             0: [Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)],
             1: [Position(0, 1), Position(1, 1), Position(2, 1), Position(2, 2)],
             2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)],
             3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)]
         }
+
+
+class JBlock(Block):
+    def __init__(self):
+        super().__init__(block_type=2)
+        self.cells = {
+            0: [Position(0, 0), Position(1, 0), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 1), Position(0, 2), Position(1, 1), Position(2, 1)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 2)],
+            3: [Position(0, 1), Position(1, 1), Position(2, 0), Position(2, 1)]
+        }
+
+
+class IBlock(Block):
+    def __init__(self):
+        super().__init__(block_type=3)
+        self.cells = {
+            0: [Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)],
+            1: [Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)],
+            2: [Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)],
+            3: [Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)]
+        }
+
+
+class OBlock(Block):
+    def __init__(self):
+        super().__init__(block_type=4)
+        self.cells = {
+            0: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)]
+        }
+
+
+class SBlock(Block):
+    def __init__(self):
+        super().__init__(block_type=5)
+        self.cells = {
+            0: [Position(0, 1), Position(0, 2), Position(1, 0), Position(1, 1)],
+            1: [Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 2)],
+            2: [Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1)],
+            3: [Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1)]
+        }
+
+
+class TBlock(Block):
+    def __init__(self):
+        super().__init__(block_type=6)
+        self.cells = {
+            0: [Position(0, 1), Position(1, 0), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 1)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 1)],
+            3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)]
+        }
+
+
+class ZBlock(Block):
+    def __init__(self):
+        super().__init__(block_type=7)
+        self.cells = {
+            0: [Position(0, 0), Position(0, 1), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 2), Position(1, 1), Position(1, 2), Position(2, 1)],
+            2: [Position(1, 0), Position(1, 1), Position(2, 1), Position(2, 2)],
+            3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 0)]
+        }
+        
